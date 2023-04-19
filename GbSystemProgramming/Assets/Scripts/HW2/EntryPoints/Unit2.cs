6abc993b8256ab43f51983212bded45717bfe601
Ramio7 <Dunadan2007@yandex.ru>
2023-04-19 10:24:06 +0300

    HW2 unit 2 in progress

--- GbSystemProgramming/Assets/Scripts/HW2/EntryPoints/Unit2.cs
+++ GbSystemProgramming/Assets/Scripts/HW2/EntryPoints/Unit2.cs
@@ -1,18 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using System;
+using Unity.Jobs;
+using System.Threading;
 
 public class Unit2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Vector3> _positions = new();
+    [SerializeField] private List<Vector3> _velocities = new();
+    [SerializeField] private List<Vector3> _finalPositions = new();
+    
+    private void Start()
     {
-        
-    }
+        ThreadPool.GetMaxThreads(out int workerThreads, out int completionPortThreads);
+        if (_positions.Count == _velocities.Count) throw new Exception("Make same count of positions and velocities");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        NativeArray<Vector3> _positionsContainer = new(_positions.Count, Allocator.Persistent);
+        NativeArray<Vector3> _velocitiesContainer = new(_velocities.Count, Allocator.Persistent);
+
+        _positionsContainer.CopyFrom(_positions.ToArray());
+        _velocitiesContainer.CopyFrom(_velocities.ToArray());
+
+        NativeArray<Vector3> _finalPositionsContainer = new(_positionsContainer.Length, Allocator.Persistent);
+
+        var job = new SimpleMover
+        {
+            m_positions = _positionsContainer,
+            m_velocities = _velocitiesContainer,
+            m_finalPositions = _finalPositionsContainer
+        };
+        var jobHandle = job.Schedule(_positionsContainer.Length, _positionsContainer.Length / workerThreads);
+
+        _finalPositionsContainer.CopyTo(_finalPositions.ToArray());
+
+        _positionsContainer.Dispose();
+        _velocitiesContainer.Dispose();
+        _finalPositionsContainer.Dispose();
     }
 }
