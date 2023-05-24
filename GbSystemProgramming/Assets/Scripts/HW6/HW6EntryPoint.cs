2d4ea45eaaeccdd350e1943d1403531b483a9574
Rami <Dunadan2007@yandex.ru>
2023-05-24 22:30:57 +0300

    HW7 ready

--- GbSystemProgramming/Assets/Scripts/HW6/HW6EntryPoint.cs
+++ GbSystemProgramming/Assets/Scripts/HW6/HW6EntryPoint.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class HW6EntryPoint : MonoBehaviour
 {
-    Material _material;
+    //private Material _material;
 
-    void Start()
-    {
-        _material = GetComponent<MeshRenderer>().material;
-        _material.SetColor("_Color", Color.white);
-        var collider = GetComponent<Collider>();
-        Debug.Log($"{collider.bounds.size.x}");
-        var texWidth = _material.GetFloat("_TexWidth");
-    }
+    //void Start()
+    //{
+    //    _material = GetComponent<MeshRenderer>().material;
+    //    _material.SetColor("_Color", Color.white);
+    //    var collider = GetComponent<Collider>();
+    //    Debug.Log($"{collider.bounds.size.x}");
+    //    var texWidth = _material.GetFloat("_TexWidth");
+    //}
 }
